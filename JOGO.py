1b4db186f4a05de0b8b8ad303137a5818b7ade32
lucasferro0 <89112830+lucasferro0@users.noreply.github.com>
2021-09-17 13:51:25 -0300

    Implementação de arquivos
    Implementação de arquivos. Criei uma espécie de contabilizador de pontos

--- JOGO.py
+++ JOGO.py
@@ -50,6 +50,14 @@ def continuar():
     start()
   elif continuos == "n":
     limpar()
+    contabilizado = contabilizar()
+    exibir_resultado(contabilizado)
+    # LIMPANDO DADOS DE VITÓRIAS
+    arquivo = open("users_ganhar.txt", "w")
+    arquivo.close()
+    # LIMPANDO DADOS DE DERROTAS
+    arquivo = open("users_perder.txt", "w")
+    arquivo.close()
   else:
     limpar()
     while continuos != "s" and continuos != "n":
@@ -62,15 +70,58 @@ def continuar():
         start()  # REINICIAR O JOGO
       elif continuos == "n":
         limpar()
+        contabilizado = contabilizar()
+        exibir_resultado(contabilizado)
+        # LIMPANDO DADOS DE VITÓRIAS
+        arquivo = open("users_ganhar.txt", "w")
+        arquivo.close()
+        # LIMPANDO DADOS DE DERROTAS
+        arquivo = open("users_perder.txt", "w")
+        arquivo.close()
       else:
         limpar()
+def login():
+  nome = input("Digite seu nome: ")
+  return nome
+def recorde_ganhar():
+  arquivo = open("users_ganhar.txt", "a")
+  arquivo.write(nome+"\n")
+  arquivo.close()
+def recorde_perder():
+  arquivo = open("users_perder.txt", "a")
+  arquivo.write(nome+"\n")
+  arquivo.close()
+
+def contabilizar():
+  # VITÓRIAS
+  arquivo = open("users_ganhar.txt", "r")
+  v = 0
+  for e in arquivo:
+    v += 1
+  arquivo.close()
+
+  # DERROTAS
+  arquivo = open("users_perder.txt", "r")
+  d = 0
+  for e in arquivo:
+    d += 1
+  arquivo.close()
+  return (v, d)
+
+def exibir_resultado(contabilizado):
+  venceu = contabilizado[0]
+  perdeu = contabilizado[1]
+  final = venceu - perdeu
+  final = str(final)
+  print(nome+", sua pontuação foi:", final+".")
+
 # INICIAR O JOGO
 def start():
     manual = jogador()
     pc = computador()
     if manual == pc:
       limpar()
-      print("Empate !\nVocê jogou",manual,"\nComputador jogou",pc+".")
+      print("Empate !\n\nVocê jogou",manual,"\nComputador jogou",pc+".")
       print("\n\nReiniciando o jogo ...")
       time.sleep(5)
       limpar()
@@ -78,40 +129,64 @@ def start():
     # JOGADOR PERDER
     elif manual == "pedra" and pc == "papel":
       limpar()
+      print("========================")
       print("Você jogou",manual+".")
       print("Computador jogou",pc+".")
+      print("========================")
       print("VOCÊ PERDEU.")
+      print("========================")
+      recorde_perder()
       continuar()
     elif manual == "papel" and pc == "tesoura":
       limpar()
+      print("========================")
       print("Você jogou",manual+".")
       print("Computador jogou",pc+".")
+      print("========================")
       print("VOCÊ PERDEU.")
+      print("========================")
+      recorde_perder()
       continuar()
     elif manual == "tesoura" and pc == "pedra":
       limpar()
+      print("========================")
       print("Você jogou",manual+".")
       print("Computador jogou",pc+".")
+      print("========================")
       print("VOCÊ PERDEU.")
+      print("========================")
+      recorde_perder()
       continuar()
     # JOGADOR GANHAR
     elif manual == "papel" and pc == "pedra":
       limpar()
+      print("========================")
       print("Você jogou",manual+".")
       print("Computador jogou",pc+".")
+      print("========================")
       print("VOCÊ GANHOU !!!.")
+      print("========================")
+      recorde_ganhar()
       continuar()
     elif manual == "tesoura" and pc == "papel":
       limpar()
+      print("========================")
       print("Você jogou",manual+".")
       print("Computador jogou",pc+".")
+      print("========================")
       print("VOCÊ GANHOU !!!.")
+      print("========================")
+      recorde_ganhar()
       continuar()
     elif manual == "pedra" and pc == "tesoura":
       limpar()
+      print("========================")
       print("Você jogou",manual+".")
       print("Computador jogou",pc+".")
+      print("========================")
       print("VOCÊ GANHOU !!!.")
+      print("========================")
+      recorde_ganhar()
       continuar()
     else:
       limpar()
@@ -121,4 +196,12 @@ def start():
       start()  # REINICIAR O JOGO
 
 # INÍCIO DO JOGO
+nome = login()
+# RESETANDO OS ARQUIVOS
+arquivo = open("users_ganhar.txt", "w")
+arquivo.close()
+arquivo = open("users_perder.txt", "w")
+arquivo.close()
+limpar()
+# JOGO
 start()
